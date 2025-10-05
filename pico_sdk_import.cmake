2c3e8a3e753222353d65d6af5da6da797da585c9
rigr <richard.grauss@gmail.com>
2025-10-05 11:15:31 +0200

    Update pico_sdk_import.cmake
    tusb was not found

--- pico_sdk_import.cmake
+++ pico_sdk_import.cmake
@@ -1,8 +1,9 @@
 # File: pico_sdk_import.cmake
 # Diese Datei sorgt dafür, dass CMake das Pico SDK findet
-
+# File: pico_sdk_import.cmake
 if (NOT PICO_SDK_PATH)
   set(PICO_SDK_PATH "${CMAKE_CURRENT_LIST_DIR}/pico-sdk")
 endif()
 
 include(${PICO_SDK_PATH}/external/pico_sdk_import.cmake)
+
